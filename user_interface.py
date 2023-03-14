ffb7d2338053e1c243dfe0ad3f66a2acc06625ad
IlyaChef <ilyaromanov89@gmail.com>
2023-03-14 21:43:14 +0300

    try to correct types

--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,6 @@
 from constants import BOARD_SIZE
 from custom_types import Symbol
+from typing import Union
 
 
 def display_welcome_message() -> None:
@@ -12,7 +13,7 @@ def get_player_name() -> str:
     return name
 
 
-def display_board(board: list[list[str]]) -> None:
+def display_board(board: list[list[Union[Symbol, str]]]) -> None:
     for row in range(BOARD_SIZE):
         print(Symbol.EMPTY.value + " | ".join(str(symbol) for symbol in board[row]) + Symbol.EMPTY.value)
         if row != BOARD_SIZE - 1:
